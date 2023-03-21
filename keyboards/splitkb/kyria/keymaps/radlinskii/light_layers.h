097510b02f5856d8f1ce07d6aecd848b17d51cff
radlinskii <radlinskiignacy@gmail.com>
2023-03-21 21:16:09 +0100

    feat: move reset keys

--- keyboards/splitkb/kyria/keymaps/radlinskii/light_layers.h
+++ keyboards/splitkb/kyria/keymaps/radlinskii/light_layers.h
@@ -1,6 +1,8 @@
 // Copyright 2022 Ignacy Radliński (@radlinskii)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+// clang-format off
+
 const rgblight_segment_t PROGMEM COLEMAK_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 19, 100, 255, 255}
 );
@@ -25,7 +27,7 @@ const rgblight_segment_t PROGMEM MOUSE_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 19, 43, 255, 255}
 );
 
-const rgblight_segment_t PROGMEM MEDIA_MISC_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
+const rgblight_segment_t PROGMEM MEDIA_FN_LIGHT_LAYER[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 19, 0, 255, 255}
 );
 
@@ -36,5 +38,5 @@ const rgblight_segment_t* const PROGMEM MY_LIGHT_LAYERS[] = RGBLIGHT_LAYERS_LIST
     SYM_LIGHT_LAYER,
     NAV_LIGHT_LAYER,
     MOUSE_LIGHT_LAYER,
-    MEDIA_MISC_LIGHT_LAYER
+    MEDIA_FN_LIGHT_LAYER
 );
