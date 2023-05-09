1ed9e1d28a1065e4635162d6f6916c6931bf1966
radlinskii <radlinskiignacy@gmail.com>
2023-05-09 22:43:34 +0200

    feat(split2): add rotary encoder support

--- keyboards/split2/keymaps/default/keymap.c
+++ keyboards/split2/keymaps/default/keymap.c
@@ -7,3 +7,15 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_HOME] = LAYOUT(KC_X, KC_D)};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    }
+
+    return false;
+}
