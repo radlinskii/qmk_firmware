3ee3fd79e405c01de11a9fefa0ed79df4b6ab1cd
radlinskii <radlinskiignacy@gmail.com>
2023-05-09 22:43:34 +0200

    feat(split2): add rotary encoder support

--- keyboards/split2/config.h
+++ keyboards/split2/config.h
@@ -9,16 +9,29 @@
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+// #define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+// #define NO_PRINT
 
 /* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
 
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
+
+#define ENCODERS_PAD_A \
+    {}
+#define ENCODERS_PAD_B \
+    {}
+#define ENCODER_RESOLUTIONS \
+    {}
+#define ENCODERS_PAD_A_RIGHT \
+    { GP14 }
+#define ENCODERS_PAD_B_RIGHT \
+    { GP15 }
+#define ENCODER_RESOLUTIONS_RIGHT \
+    { 4 }
